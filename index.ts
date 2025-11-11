182025bf82a582bad6edaa28113aaafb991f6751
Romain Lanz <romain.lanz@pm.me>
2025-11-11 21:28:31 +0100

    chore: add more export for easy driver creation

--- index.ts
+++ index.ts
@@ -6,4 +6,7 @@
  */
 
 export { EncryptionManager } from './src/encryption_manager.ts'
+export { Hmac } from './src/hmac.ts'
+export { BaseDriver } from './src/drivers/base_driver.ts'
+export { base64UrlDecode, base64UrlEncode } from './src/base64.ts'
 export * as errors from './src/exceptions.ts'
